ca1ec17fa7bdea8bf108679548d5957c152d41f4
jroper7 <josephroper57@gmail.com>
2024-10-13 23:45:46 -0400

    Trying to get the login logic working

--- app/app.js
+++ app/app.js
@@ -1,47 +1,61 @@
-import {loadPage, getBooks} from "../model/model.js";
+import { loadPage, signIn, signUp } from "../model/model.js";
+
+let signedIn = false; // Ensure signedIn is initialized and accessible
 
 const changeRoute = () => {
     let hashTag = window.location.hash;
     let pageID = hashTag.replace('#', '') || 'home';
 
     loadPage(pageID);
-
 }
 
-
 function initURLListener() {
+    console.log("Initializing URL listener");  
     $(window).on('hashchange', changeRoute);
     changeRoute();
 }
 
-
 $(document).ready(function () {
-<<<<<<< HEAD
-    loadPage("febFavs")
-=======
-    loadPage("home")
+    console.log("Document is ready"); 
+    
+    $(".loginBtn a, .signupBtn a").on("click", function (e) {
+        e.preventDefault(); 
+        var url = $(this).attr("href").replace("#", ""); 
+
+        console.log("Clicked button, url: ", url); 
+
+        if (url == "home") {
+            if ($(this).parent().hasClass("loginBtn")) {
+                alert("About to call signIn");
+            } else if ($(this).parent().hasClass("signupBtn")) {
+                alert("About to call signIn");
+            }
+        }
+    });
+    loadPage("home");
     initURLListener();
-    
-    
+   
 });
 
-const loadFeaturedBooks = () => {
-    let books = getBooks();  
-    console.log("loading books")
-    console.log(books); 
-    console.log($("#feature-holder").length);
-    console.log($("#home-holder").html()); 
-    $.each(books, function (index, book) {
-        $("#feature-holder").append(
-            `<div class="book"></div>
-             <div class="book-info">
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." Urna nunc id cursus metus aliquam eleifend mi. Massa vitae tortor condimentum lacinia quis vel eros.
-                </p>
-             </div>
-            <div class="price">${book.price}</div>
-            <div className="add"></div>
-            `
-        );
-    });
-};
+
+
+// const loadFeaturedBooks = () => {
+//     let books = getBooks();  
+//     console.log("loading books")
+//     console.log(books); 
+//     console.log($("#feature-holder").length);
+//     console.log($("#home-holder").html()); 
+//     $.each(books, function (index, book) {
+//         $("#feature-holder").append(
+//             `<div class="book"></div>
+//              <div class="book-info">
+//                 <p>
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." Urna nunc id cursus metus aliquam eleifend mi. Massa vitae tortor condimentum lacinia quis vel eros.
+//                 </p>
+//              </div>
+//             <div class="price">${book.price}</div>
+//             <div className="add"></div>
+//             `
+//         );
+//     });
+// };
--- model/model.js
+++ model/model.js
@@ -14,89 +14,130 @@ export const loadPage = (pageID) => {
 }
 
 
-var books = [
-    
-    {
-        id: 1,
-        name: "To kill a Mocking Bird",
-        price: "15.99",
-        imageUrl: 'assets/to-kill-a-mockingbird.png'
-    },
-    {
-        id: 2,
-        name: "Becoming Michelle Obama",
-        price: "25.99",
+let signedIn = false; 
 
-    },
-    {
-        id: 3,
-        name: "Fire Starter",
-        price: "19.99",
-    },
-    {
-        id: 4,
-        name: "RedBox",
-        price: ".99",
-    },
-    {
-        id: 5,
-        name: "Mockingjay",
-        price: "10.99",
-    },
-    {
-        id: 6,
-        name: "Html",
-        price: "9.99",
-    },
-    {
-        id: 7,
-        name: "redBox",
-        price: "9.99",
-    },
-    {
-        id: 8,
-        name: "Harry Potter Collection",
-        price: "9.99",
-    },
-    {
-        id: 9,
-        name: "George Martin",
-        price: "9.99",
-    },
-    {
-        id: 10,
-        name: "Html",
-        price: "9.99",
-    },
-    {
-        id: 11,
-        name: "Html",
-        price: "9.99",
-    },
-    {
-        id: 12,
-        name: "Html",
-        price: "9.99",
-    },
-    {
-        id: 13,
-        name: "Html",
-        price: "9.99",
-    },
 
-    {
-        id: 14,
-        name: "Html",
-        price: "9.99",
-    },
-    {
-        id: 15,
-        name: "Red Box",
-        price: "9.99",
+export const signIn = () => {
+    const email = $("#emailInputLogin").val();
+    const password = $("#passwordInputLogin").val();
+
+    if (email && password) {
+        signedIn = true;
+        alert("You are logged in!");
+        window.location.href = "#home"; 
+        console.log(email, password)
+        return signedIn;
+    } else {
+        alert("Please enter valid login details!");
+        return false;
     }
-];
+};
 
 
-export const getBooks = () => {
-    return books
-}
+export const signUp = () => {
+    const firstName = $("#firstNameInput").val();
+    const lastName = $("#lastNameInput").val();
+    const email = $("#emailInput").val();
+    const password = $("#passwordInput").val();
+
+   
+    if (firstName && lastName && email && password) {
+        console.log("First Name: ", firstName); 
+        alert(`Thank you for signing up, ${firstName} ${lastName}!`);
+        signedIn = true;
+        window.location.href = "#home"; 
+        return signedIn;
+    } else {
+        alert("Please complete all sign-up fields!");
+        return false;
+    }
+};
+
+
+
+// var books = [
+    
+//     {
+//         id: 1,
+//         name: "To kill a Mocking Bird",
+//         price: "15.99",
+//         imageUrl: 'assets/to-kill-a-mockingbird.png'
+//     },
+//     {
+//         id: 2,
+//         name: "Becoming Michelle Obama",
+//         price: "25.99",
+
+//     },
+//     {
+//         id: 3,
+//         name: "Fire Starter",
+//         price: "19.99",
+//     },
+//     {
+//         id: 4,
+//         name: "RedBox",
+//         price: ".99",
+//     },
+//     {
+//         id: 5,
+//         name: "Mockingjay",
+//         price: "10.99",
+//     },
+//     {
+//         id: 6,
+//         name: "Html",
+//         price: "9.99",
+//     },
+//     {
+//         id: 7,
+//         name: "redBox",
+//         price: "9.99",
+//     },
+//     {
+//         id: 8,
+//         name: "Harry Potter Collection",
+//         price: "9.99",
+//     },
+//     {
+//         id: 9,
+//         name: "George Martin",
+//         price: "9.99",
+//     },
+//     {
+//         id: 10,
+//         name: "Html",
+//         price: "9.99",
+//     },
+//     {
+//         id: 11,
+//         name: "Html",
+//         price: "9.99",
+//     },
+//     {
+//         id: 12,
+//         name: "Html",
+//         price: "9.99",
+//     },
+//     {
+//         id: 13,
+//         name: "Html",
+//         price: "9.99",
+//     },
+
+//     {
+//         id: 14,
+//         name: "Html",
+//         price: "9.99",
+//     },
+//     {
+//         id: 15,
+//         name: "Red Box",
+//         price: "9.99",
+//     }
+// ];
+
+
+// export const getBooks = () => {
+//     return books
+// }
